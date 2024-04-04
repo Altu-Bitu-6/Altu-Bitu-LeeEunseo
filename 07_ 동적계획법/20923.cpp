46afdd23e50c1b13c264d9550785abd66e96226a
monimoni12 <monimoni@ewhain.net>
2024-04-04 23:57:22 +0900

    [동적계획법] 4월 4일

--- 07_ 동적계획법/20923.cpp
+++ 07_ 동적계획법/20923.cpp
@@ -1,29 +1,75 @@
-#include <iostream> //자료 구조, 덱, 구현, 시뮬레이션
-#include <queue>
-#include <vector>
+#include <iostream>
+#include <deque>
+#include <stack>
+#include <algorithm>
+
 using namespace std;
 
+void RingTheBell(stack<int>& winGround, stack<int>& loseGround, deque<int>& Deque) {
+    while (!winGround.empty()) {
+        Deque.push_front(winGround.top());
+        winGround.pop();
+    }
+    while (!loseGround.empty()) {
+        Deque.push_back(loseGround.top());
+        loseGround.pop();
+    }
+}
 
-int main(){
-    int N;
-    cin >> N;
+void HalliGalli(deque<int>& doDeque, deque<int>& suDeque, int M) {
+    stack<int> doGround; // 도도의 그라운드 스택
+    stack<int> suGround; // 수연이의 그라운드 스택
 
-    priority_queue<int, vector<int>, greater<int> > minHeap; // 최소 힙
+    int rounds = min(M, min(static_cast<int>(doDeque.size()), static_cast<int>(suDeque.size())));
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            int num;
-            cin >> num;
-            minHeap.push(num);
-            // 가장 작은 원소를 제거해서 "가장 큰 N개 숫자" 유지
-            if (minHeap.size() > N) {
-                minHeap.pop();
+    for(int i = 0; i < rounds; ++i) {
+        doGround.push(doDeque.front()); // 도도 그라운드에 카드 내려놓기
+        doDeque.pop_front(); // 도도 덱에서 카드 삭제하기
+        suGround.push(suDeque.front()); // 수연이 그라운드에 카드 내려놓기
+        suDeque.pop_front(); // 수연이 덱에서 카드 삭제하기
+
+        if (!doGround.empty() && !suGround.empty()) {
+            if (doGround.top() + suGround.top() == 5) { // 수연이가 종 칠 때
+                RingTheBell(suGround, doGround, suDeque);
+            } else if (doGround.top() == 5 || suGround.top() == 5) { // 도도가 종 칠 때
+                RingTheBell(doGround, suGround, doDeque);
             }
         }
     }
 
-    // N번째로 큰 수 출력
-    cout << minHeap.top() << "\n";
+    int doRemaining = doDeque.size(); // 남은 도도의 카드 수
+    int suRemaining = suDeque.size(); // 남은 수연의 카드 수
+
+    if (doRemaining == 0 && suRemaining == 0) { // 둘 다 비었을 경우
+        cout << "dosu" << "\n";
+    } else if (doRemaining == 0) { // 도도의 덱만 비었을 경우
+        cout << "su" << "\n";
+    } else if (suRemaining == 0) { // 수연의 덱만 비었을 경우
+        cout << "do" << "\n";
+    } else if (doRemaining > suRemaining) { // 게임 진행 후 도도의 덱에 카드가 더 많을 경우
+        cout << "do" << "\n";
+    } else if (doRemaining < suRemaining) { // 게임 진행 후 수연의 덱에 카드가 더 많을 경우
+        cout << "su" << "\n";
+    } else { // 덱의 크기가 같을 경우
+        cout << "dosu" << "\n"; // 비긴 경우 "dosu" 출력
+    }
+}
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    deque<int> doDeque; // 도도 덱
+    deque<int> suDeque; // 수연이 덱
+
+    for(int i = 0; i < N; ++i) {
+        int dodo, suyeon;
+        cin >> dodo >> suyeon;
+        doDeque.push_back(dodo);
+        suDeque.push_back(suyeon);
+    }
+
+    HalliGalli(doDeque, suDeque, M);
 
     return 0;
 }
